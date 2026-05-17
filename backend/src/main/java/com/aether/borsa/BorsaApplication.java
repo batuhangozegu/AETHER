8a5ed90c81e276529dcb8759ff11ef29f60eaba7
Batuhan <batuhangozegu@gmail.com>
2026-05-17 14:52:42 +0300

    Proje kurulumu tamamlandı

---
+++ backend/src/main/java/com/aether/borsa/BorsaApplication.java
@@ -0,0 +1,13 @@
+package com.aether.borsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BorsaApplication.class, args);
+	}
+
+}
