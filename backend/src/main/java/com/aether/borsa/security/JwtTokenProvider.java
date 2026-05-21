d709d8e6d635cb5a30ff279df1fa26892886537c
Batuhan <batuhangozegu@gmail.com>
2026-05-21 23:05:33 +0300

    fix: JwtException import eklendi

--- backend/src/main/java/com/aether/borsa/security/JwtTokenProvider.java
+++ backend/src/main/java/com/aether/borsa/security/JwtTokenProvider.java
@@ -1,6 +1,7 @@
 package com.aether.borsa.security;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
