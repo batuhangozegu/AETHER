26a2a3bb6864df186235ea1e4bccadc7d80b4acd
Batuhan <batuhangozegu@gmail.com>
2026-06-02 22:17:00 +0300

    feat: UserDetailsServiceImpl eklendi

---
+++ backend/src/main/java/com/aether/borsa/security/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.aether.borsa.security;
+
+import com.aether.borsa.model.entity.User;
+import com.aether.borsa.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public  UserDetailsServiceImpl(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username).orElseThrow( () -> new UsernameNotFoundException("Kullanıcı Bulunamadı" + username));
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPasswordHash())
+                .authorities(new ArrayList<>())
+                .build();
+    }
+}
