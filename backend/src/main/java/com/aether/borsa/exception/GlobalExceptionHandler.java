f4b8f62fee5f5e27e5150e6db5c967eab4aa20ec
Batuhan <batuhangozegu@gmail.com>
2026-06-24 16:26:54 +0300

    feat: GlobalExceptionHandler eklendi

---
+++ backend/src/main/java/com/aether/borsa/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.aether.borsa.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex){
+        return ResponseEntity.status(400).body(ex.getMessage());
+    }
+}
